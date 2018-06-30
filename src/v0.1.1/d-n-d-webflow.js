420ad1ba6307565323a8397a1734bba7092c4e41
Daniles <danilmm@mail.ru>
2018-06-30 17:14:15 +0300

    Added v0.1.1

---
+++ src/v0.1.1/d-n-d-webflow.js
@@ -0,0 +1,32 @@
+/**
+ * d-n-d-webflow.js 0.1.1
+ * https://github.com/Daniles/d-n-d-webflow
+ *
+ * (c) 2018+ Daniles
+ */
+
+
+var dndScrollEvent = new Event("scroll", {
+    target: document
+  });
+document.onscroll = function(e) {
+    dndScrollEvent = e;
+};
+
+function InitDNDWebflow(sliderId, scrollableSubstrate) {
+    $(function() {
+        new Dragdealer(sliderId, {
+            animationCallback: function(x, y) {
+                $(scrollableSubstrate).css( "top", (100 - x * 100) + "vh");
+                DispathEvent();
+            }
+        });
+    })
+}
+
+function DispathEvent(){
+    if (dndScrollEvent == null)
+      return;
+    var new_event = new Event(dndScrollEvent.type, dndScrollEvent);
+    document.dispatchEvent(new_event);
+}
